d9132edc93a6d48791d310cdfeaca865e0d8c8d8
Rukshona Rad <rukshona.rad@gmail.com>
2023-12-14 22:04:41 -0500

    Implement some logic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React from "react";
-import "./App.css";
 import { v4 as uuid } from "uuid";
-const todos = [{ id: 1, todo: "text", isDone: true }];
+import "./App.css";
 class App extends React.Component {
     constructor() {
         super();
         this.state = {
             todos: [],
-            inputValue: ""
+            inputValue: "",
+            inputError: false,
+            showEditModal: false,
+            inputEditValue: "",
+            editingTodoId: ""
         };
     }
-
     addTodo = (e) => {
         e.preventDefault();
-        console.log(this.state.inputValue);
+        if (this.state.inputValue.length <= 1) {
+            this.setState({
+                inputError: false
+            });
+            return;
+        }
         const newTodo = {
             id: uuid(),
             text: this.state.inputValue,
             isDone: false
         };
-
         this.setState((prevState) => {
             const copyTodos = [...prevState.todos, newTodo];
             return {
@@ -28,7 +34,6 @@ class App extends React.Component {
             };
         });
     };
-
     updateTodo = (isDone, todoId) => {
         this.setState((prevState) => {
             const updatedTodos = prevState.todos.map((todo) => {
@@ -38,18 +43,6 @@ class App extends React.Component {
                 }
                 return todo;
             });
-
-            return {
-                todos: updatedTodos
-            };
-        });
-    };
-    deleteTodo = (todoId) => {
-        this.setState((prevState) => {
-            const updatedTodos = prevState.todos.filter((todo) => {
-                return todo.id !== todoId;
-            });
-
             return {
                 todos: updatedTodos
             };
@@ -60,18 +53,75 @@ class App extends React.Component {
         this.setState({
             inputValue: value
         });
+        if (value.length <= 1) {
+            this.setState({
+                inputError: true
+            });
+        } else {
+            this.setState({
+                inputError: false
+            });
+        }
     };
-
+    deleteTodo = (todoId) => {
+        this.setState((prevState) => {
+            const keptTodos = prevState.todos.filter(
+                (todo) => todo.id !== todoId
+            );
+            return {
+                todos: keptTodos
+            };
+        });
+    };
+    editTodo = (todoId) => {
+        this.setState({
+            showEditModal: true
+        });
+        let todoText = "";
+        for (const todo of this.state.todos) {
+            if (todo.id === todoId) {
+                todoText = todo.text;
+                break;
+            }
+        }
+        this.setState({
+            inputEditValue: todoText,
+            editingTodoId: todoId
+        });
+    };
+    handleInputEdit = (e) => {
+        this.setState({
+            inputEditValue: e.target.value
+        });
+    };
+    submitEdit = () => {
+        this.setState((prevState) => {
+            const updatedTodos = prevState.todos.map((todo) => {
+                if (todo.id === this.state.editingTodoId) {
+                    const copy = { ...todo, text: this.state.inputEditValue };
+                    return copy;
+                }
+                return todo;
+            });
+            return {
+                todos: updatedTodos,
+                showEditModal: false
+            };
+        });
+    };
     render() {
         return (
             <main>
                 <form onSubmit={this.addTodo}>
-                    <input
-                        onChange={this.handleOnChange}
-                        value={this.state.inputValue}
-                        type="text"
-                        placeholder="What is your mind"
-                    />
+                    <div className="form-control">
+                        <input
+                            onChange={this.handleOnChange}
+                            value={this.state.inputValue}
+                            type="text"
+                            placeholder="What is on your mind"
+                        />
+                        {this.state.inputError && <span>Invalid Todo</span>}
+                    </div>
                     <input type="submit" value="Add Todo" />
                 </form>
                 <ul>
@@ -98,15 +148,28 @@ class App extends React.Component {
                                     <button
                                         onClick={() => this.deleteTodo(todo.id)}
                                     >
-                                        Delete Todo
+                                        X
+                                    </button>
+                                    <button
+                                        onClick={() => this.editTodo(todo.id)}
+                                    >
+                                        Edit
                                     </button>
                                 </li>
                             );
                         })}
                 </ul>
+                this.state.showEditModal && (
+                <div className="modal">
+                    <input
+                        value={this.state.inputEditValue}
+                        onChange={this.handleInputEdit}
+                    />
+                    <button onClick={this.submitEdit}>Update Todo</button>
+                </div>
+                )
             </main>
         );
     }
 }
-
 export default App;
