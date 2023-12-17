f755832dff9f8ab2e86c045a01c7d516bb029a9c
Rukshona Rad <rukshona.rad@gmail.com>
2023-12-17 07:35:40 -0500

    Convert to function component

--- src/AppFunction.js
+++ src/AppFunction.js
@@ -1,211 +1,136 @@
-import React from "react";
+import React, { useState } from "react";
 import { v4 as uuid } from "uuid";
 import Dialog from "@mui/material/Dialog";
 import { DialogTitle } from "@mui/material";
 import "./App.css";
 
-class App extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            todos: [],
-            inputValue: "",
-            inputError: false,
-            showEditModal: false,
-            inputEditValue: "",
-            editingTodoId: ""
-        };
-    }
+const AppFunction = () => {
+    const [todos, setTodos] = useState([]);
+    const [inputValue, setInputValue] = useState("");
+    const [inputError, setInputError] = useState(false);
+    const [showEditModal, setShowEditModal] = useState(false);
+    const [inputEditValue, setInputEditValue] = useState("");
+    const [editingTodoId, setEditingTodoId] = useState("");
 
-    addTodo = (e) => {
+    const addTodo = (e) => {
         e.preventDefault();
 
-        if (this.state.inputValue.length <= 1) {
-            this.setState({
-                inputError: true
-            });
-
+        if (inputValue.length <= 1) {
+            setInputError(true);
             return;
         }
         const newTodo = {
             id: uuid(),
-            text: this.state.inputValue,
+            text: inputValue,
             isDone: false
         };
 
-        this.setState((prevState) => {
-            const copyTodos = [...prevState.todos, newTodo];
-            return {
-                todos: copyTodos,
-                inputValue: ""
-            };
-        });
+        setTodos((prevState) => [...prevState, newTodo]);
+        setInputValue("");
     };
 
-    updateTodo = (isDone, todoId) => {
-        this.setState((prevState) => {
-            const updatedTodos = prevState.todos.map((todo) => {
-                if (todo.id === todoId) {
-                    const copy = { ...todo, isDone: isDone };
-                    return copy;
-                }
-                return todo;
-            });
-
-            return {
-                todos: updatedTodos
-            };
-        });
+    const updateTodo = (isDone, todoId) => {
+        setTodos((prevState) =>
+            prevState.map((todo) =>
+                todo.id === todoId ? { ...todo, isDone: isDone } : todo
+            )
+        );
     };
 
-    handleOnChange = (e) => {
+    const handleOnChange = (e) => {
         const { value } = e.target;
-        this.setState({
-            inputValue: value
-        });
+        setInputValue(value);
 
         if (value.length <= 1) {
-            this.setState({
-                inputError: true
-            });
+            setInputError(true);
         } else {
-            this.setState({
-                inputError: false
-            });
+            setInputError(false);
         }
     };
 
-    deleteTodo = (todoId) => {
-        this.setState((prevState) => {
-            const keptTodos = prevState.todos.filter(
-                (todo) => todo.id !== todoId
-            );
-            return {
-                todos: keptTodos
-            };
-        });
+    const deleteTodo = (todoId) => {
+        setTodos((prevState) => prevState.filter((todo) => todo.id !== todoId));
     };
 
-    editTodo = (todoId) => {
-        this.setState({
-            showEditModal: true
-        });
-        let todoText = "";
-        for (const todo of this.state.todos) {
-            if (todo.id === todoId) {
-                todoText = todo.text;
-                break;
-            }
-        }
+    const editTodo = (todoId) => {
+        setShowEditModal(true);
+        const todo = todos.find((todo) => todo.id === todoId);
 
-        this.setState({
-            inputEditValue: todoText,
-            editingTodoId: todoId
-        });
+        setInputEditValue(todo.text);
+        setEditingTodoId(todoId);
     };
 
-    handleInputEdit = (e) => {
-        this.setState({
-            inputEditValue: e.target.value
-        });
+    const handleInputEdit = (e) => {
+        setInputEditValue(e.target.value);
     };
 
-    submitEdit = () => {
-        this.setState((prevState) => {
-            const updatedTodos = prevState.todos.map((todo) => {
-                if (todo.id === this.state.editingTodoId) {
-                    const copy = { ...todo, text: this.state.inputEditValue };
-                    return copy;
-                }
-                return todo;
-            });
-
-            return {
-                todos: updatedTodos,
-                showEditModal: false
-            };
-        });
-    };
-
-    closeModal = () => {
-        this.setState({ showEditModal: false });
-    };
-
-    render() {
-        return (
-            <main>
-                <form onSubmit={this.addTodo}>
-                    <div className="form-control">
-                        <input
-                            onChange={this.handleOnChange}
-                            value={this.state.inputValue}
-                            type="text"
-                            placeholder="What is your mind"
-                        />
-                        {this.state.inputError && <span>Invalid Todo</span>}
-                    </div>
-
-                    <input type="submit" value="Add Todo" />
-                </form>
-                <ul>
-                    {this.state.todos.length >= 1 &&
-                        this.state.todos.map((todo) => {
-                            return (
-                                <li key={todo.id} className="todo">
-                                    <span
-                                        className={
-                                            todo.isDone
-                                                ? "todo__text--done"
-                                                : ""
-                                        }
-                                    >
-                                        {todo.text}
-                                    </span>
-
-                                    <input
-                                        type="checkbox"
-                                        defaultChecked={todo.isDone}
-                                        onChange={(e) => {
-                                            this.updateTodo(
-                                                e.target.checked,
-                                                todo.id
-                                            );
-                                        }}
-                                    />
-                                    <button
-                                        onClick={() => this.deleteTodo(todo.id)}
-                                    >
-                                        X
-                                    </button>
-                                    <button
-                                        onClick={() => this.editTodo(todo.id)}
-                                    >
-                                        Edit
-                                    </button>
-                                </li>
-                            );
-                        })}
-                </ul>
-
-                <Dialog
-                    open={this.state.showEditModal}
-                    onClose={this.closeModal}
-                >
-                    <div className="edit-form">
-                        <DialogTitle id="alert-dialog-title">
-                            Edit Todo
-                        </DialogTitle>
-                        <input
-                            value={this.state.inputEditValue}
-                            onChange={this.handleInputEdit}
-                        />
-                        <button onClick={this.closeModal}>Cancel</button>
-                        <button onClick={this.submitEdit}> Update Todo</button>
-                    </div>
-                </Dialog>
-            </main>
+    const submitEdit = () => {
+        setTodos((prevState) =>
+            prevState.map((todo) =>
+                todo.id === editingTodoId
+                    ? { ...todo, text: inputEditValue }
+                    : todo
+            )
         );
-    }
-}
+        setShowEditModal(false);
+    };
 
-export default App;
+    const closeModal = () => {
+        setShowEditModal(false);
+    };
+
+    return (
+        <main>
+            <form onSubmit={addTodo}>
+                <div className="form-control">
+                    <input
+                        onChange={handleOnChange}
+                        value={inputValue}
+                        type="text"
+                        placeholder="What is on your mind"
+                    />
+                    {inputError && <span>Invalid Todo</span>}
+                </div>
+                <input type="submit" value="Add Todo" />
+            </form>
+            <ul>
+                {todos.length >= 1 &&
+                    todos.map((todo) => (
+                        <li key={todo.id} className="todo">
+                            <span
+                                className={
+                                    todo.isDone ? "todo__text--done" : ""
+                                }
+                            >
+                                {todo.text}
+                            </span>
+                            <input
+                                type="checkbox"
+                                defaultChecked={todo.isDone}
+                                onChange={(e) => {
+                                    updateTodo(e.target.checked, todo.id);
+                                }}
+                            />
+                            <button onClick={() => deleteTodo(todo.id)}>
+                                X
+                            </button>
+                            <button onClick={() => editTodo(todo.id)}>
+                                Edit
+                            </button>
+                        </li>
+                    ))}
+            </ul>
+
+            <Dialog open={showEditModal} onClose={closeModal}>
+                <div className="edit-form">
+                    <DialogTitle id="alert-dialog-title">Edit Todo</DialogTitle>
+                    <input value={inputEditValue} onChange={handleInputEdit} />
+                    <button onClick={closeModal}>Cancel</button>
+                    <button onClick={submitEdit}>Update Todo</button>
+                </div>
+            </Dialog>
+        </main>
+    );
+};
+
+export default AppFunction;
